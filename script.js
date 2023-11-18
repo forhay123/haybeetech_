6cdbe3b95d405fe74dffbdfed0073f99fa3f0080
forhay123 <148324572+forhay123@users.noreply.github.com>
2023-11-18 09:36:23 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+var typed = new Typed(".multiple-text", {
+    strings: ["OLIHA HENRY ABRAHAM", "You Are Welcome"],
+typeSpeed: 100,
+backSpeed:100,
+backDelay: 1000,
+loop: true
+})
+
+var typed = new Typed(".multiple-text2", {
+    strings: ["OLIHA HENRY ABRAHAM", "You Are Welcome"],
+typeSpeed: 100,
+backSpeed:100,
+backDelay: 1000,
+loop: true
+})
